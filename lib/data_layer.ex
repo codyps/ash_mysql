fd2414b1d1a8e74978944f884479920add5ad61b
Cody P Schafer <dev@codyps.com>
2026-01-29 12:53:15 -0500

    fix: allow transactions

--- lib/data_layer.ex
+++ lib/data_layer.ex
@@ -389,7 +389,7 @@ defmodule AshMysql.DataLayer do
   def can?(_, :bulk_create), do: true
   def can?(_, {:lock, _}), do: false
 
-  def can?(_, :transact), do: false
+  def can?(_, :transact), do: true
   def can?(_, :composite_primary_key), do: true
   def can?(_, {:atomic, :update}), do: true
   def can?(_, {:atomic, :upsert}), do: false
